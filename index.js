5549693c3fa34dbb5ef216e6a82fd4aeebb6a57f
Romo_Gun <renathor97@gmail.com>
2025-04-23 22:47:04 -0600

    React App ready

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const path = require('path');
 const express = require('express');//es como una importacion 
 require('dotenv').config();
 const cors = require('cors');
 const { dbConection } = require('./database/config');
+const { patch } = require('./routes/auth');
 
 // crear el servidor de express 
 const app = express();
@@ -21,6 +23,10 @@ app.use( express.json() );
 // Rutas
 app.use( '/api/auth', require('./routes/auth') );//todo lo que aporta el archivo se exporta en la primer ruta 
 app.use('/api/events', require('./routes/events') );
+
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 // escuchar peticioones 
 app.listen( process.env.PORT, () => {
